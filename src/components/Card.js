b6f9c743c87eb9a6d0bcaf680a2e159f6ece28e5
RuHd <ruaan.ram@gmail.com>
2023-07-20 13:24:28 -0300

    feat: Card component creation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './styles/card.scss'
 
-const Card = () => {
+import flipCardSound from '../assets/SFX/flipcard-91468.mp3'
+
+const Card = ({cardName,animalImg,attackPts,animalIcon, isPlayer}) => {
+    const [isDestroyed, setisDestroyed] = useState(false)
+
     return (
-        <div>
-            
+        <div className= "card" onLoad={}>
+            <audio src={flipCardSound} autoPlay ></audio>
+            <img src={animalIcon} className='icon'/>
+            <h2>{cardName}</h2>
+            <img src={animalImg}/>
+            <section className='attack'>
+                <span>Atk</span>
+                <span>{attackPts}</span>
+            </section>
         </div>
     )
 }
